d34c1376af94d5532b57599b1a5db09f8053ba77
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2023-10-22 15:04:55 -0400

    Add connection navigation

---
+++ screens/ConnectionScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import { Input, ListItem } from "react-native-elements";
+import { styles } from "../styles";
+
+const ConnectionScreen = () => {
+    return(
+        <View>
+            <Text>Connection screen placeholder</Text>
+        </View>
+    )
+}
+
+export default ConnectionScreen;

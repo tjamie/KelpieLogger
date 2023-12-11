ec0d6c596c4a07d2e6e0c9423287a5217b7f0434
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2023-12-10 23:16:48 -0500

    Add user registration

--- screens/ConnectionScreen.js
+++ screens/ConnectionScreen.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, ScrollView, Text, Modal, Alert, FlatList } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Input, ListItem } from "react-native-elements";
 import { addToken, addUser, deleteToken } from "../reducers/settingsReducer";
+import RegistrationModal from "../components/modals/RegistrationModal";
 import { styles } from "../styles";
-import { add } from "react-native-reanimated";
 
 const ConnectionScreen = () => {
 
@@ -13,36 +13,88 @@ const ConnectionScreen = () => {
     const [port, setPort] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [tempAddress, setTempAddress] = useState("");
+    const [tempPort, setTempPort] = useState("");
+    const [tempUsername, setTempUsername] = useState("");
+    const [tempPassword, setTempPassword] = useState("");
+    const [tempConfirmPassword, setTempConfirmPassword] = useState("");
+    const [showRegistrationModal, setShowRegistrationModal] = useState(false);
     const settings = useSelector((state) => state.settings);
     const dispatch = useDispatch();
 
 
     const fillUrl = (addressInput, portInput) => {
-        const url = port.length > 0
+        const url = portInput.length > 0
             ? "http://" + addressInput + ":" + portInput
             : "http://" + addressInput;
         
-        console.log(`port length: ${port.length}`)
+        // console.log(`port length: ${port.length}`)
+        console.log(`Address: ${addressInput}`);
+        console.log(`Port: ${portInput}`);
         console.log(`Constructed URL: ${url}`);
         return url;
-    }    
+    }
 
-    // const registerUser = async() => {
-    //     const url = port.length > 0 ? "https://" + address : "https://" + address + ":" + port;
-    //     try{
-    //         const response = await fetch(url + "/api/users",
-    //         {
-    //             method: "POST",
-    //             headers:{
-    //                 "Content-Type": "application/json"
-    //             },
-    //             body: JSON.stringify({
+    const initializeRegistrationModal = () => {
+        setTempAddress(address);
+        setTempPort(port);
+        setTempUsername("");
+        setTempPassword("");
+        setTempConfirmPassword("");
+    }
 
-    //             })
-    //         }
-    //         )
-    //     }
-    // }
+    const handleRegistrationSubmit = async() => {
+        if (tempUsername.length < 5){
+            alert("Username must be at least 5 characters long.");
+        }
+        if (tempPassword !== tempConfirmPassword){
+            alert("Passwords must match.");
+            setTempPassword("");
+            setTempConfirmPassword("");
+            console.log("Passwords do not match");
+            return;
+        }
+        if (tempPassword.length < 8){
+            alert("Password must be at least 8 characters long.")
+            return;
+        }
+        console.log("Passwords match");        
+        
+        const url = fillUrl(tempAddress, tempPort)
+
+        // Attempt to post new user
+        try{
+            const response = await fetch(url + "/api/users",
+            {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: tempUsername,
+                    password: tempPassword
+                })
+            })
+            if (response.ok){
+                console.log("Registration successful");
+                setUsername(tempUsername);
+                setPassword(tempPassword);
+                setAddress(tempAddress);
+                setPort(tempPort);
+                setShowRegistrationModal(!showRegistrationModal);
+            } else {
+                const responseText = await response.text();
+                console.log(responseText);
+                const errMsg = responseText
+                    ? `HTTP error: ${response.status} ${responseText}`
+                    : `HTTP error: ${response.status}`;
+                alert(errMsg);
+            }
+        }
+        catch(error) {
+            console.error(error);
+        }
+    }
 
     const login = async() => {
         const serverUrl = fillUrl(address, port);
@@ -84,6 +136,21 @@ const ConnectionScreen = () => {
 
     return(
         <View style={styles.projectContainer}>
+            <RegistrationModal
+                showRegistrationModal = {showRegistrationModal}
+                setShowRegistrationModal = {setShowRegistrationModal}
+                tempAddress = {tempAddress}
+                setTempAddress = {setTempAddress}
+                tempPort = {tempPort}
+                setTempPort = {setTempPort}
+                tempUsername = {tempUsername}
+                setTempUsername = {setTempUsername}
+                tempPassword = {tempPassword}
+                setTempPassword = {setTempPassword}
+                tempConfirmPassword = {tempConfirmPassword}
+                setTempConfirmPassword = {setTempConfirmPassword}
+                handleRegistrationSubmit = {handleRegistrationSubmit}
+            />
             {/* Server info */}
             <Text style={styles.projectHeaderText}>Server</Text>
             <View style={{flexDirection: "row"}}>
@@ -91,6 +158,7 @@ const ConnectionScreen = () => {
                 <View style={{flex: 3}}>
                     <Text style={styles.projectText}>Address</Text>
                     <Input
+                        value = {address}
                         onChangeText={(text) => {setAddress(text)}}
                         placeholder="127.0.0.1"
                     />
@@ -100,6 +168,7 @@ const ConnectionScreen = () => {
                 <View style={{flex: 1}}>
                     <Text style={styles.projectText}>Port</Text>
                     <Input
+                        value = {port}
                         onChangeText={(text) => {setPort(text)}}
                         placeholder="443"
                     />
@@ -110,6 +179,7 @@ const ConnectionScreen = () => {
             <Text style={styles.projectHeaderText}>Account</Text>
             <Text style={styles.projectText}>Username</Text>
             <Input
+                value = {username}
                 onChangeText={(text) => {setUsername(text)}}
                 placeholder="llama"
             />
@@ -133,7 +203,12 @@ const ConnectionScreen = () => {
             
             <Button
                 title='Register'
-                onPress={() => {console.log("Register pressed")}}
+                onPress={() => {
+                    console.log("Register pressed");
+                    initializeRegistrationModal();
+                    setShowRegistrationModal(true);
+                    //setmodalvis etc
+                }}
                 buttonStyle={styles.buttonMain}
                 titleStyle={styles.buttonMainText}
             />

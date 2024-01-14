c2db83df79acb8b48f35db48a7c378d67c88a6da
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2024-01-13 21:08:06 -0500

    Replace UUID library

--- utils/createUniqueId.js
+++ utils/createUniqueId.js
@@ -1,8 +1,10 @@
-import 'react-native-get-random-values'; // required for uuid to work
-import {v4 as uuidv4} from 'uuid';
+// import 'react-native-get-random-values'; // required for uuid to work
+// import {v4 as uuidv4} from 'uuid';
+import uuid from 'react-native-uuid';
 
 export const createUniqueId = () => {
-    const idOut = uuidv4();
+    // uuid library randomly breaks as of 2024, so now using react-native-uuid
+    const idOut = uuid.v4();
     console.log(`Creating new UUID: ${idOut}`);
     return idOut;
 };
